56ebabf18b61b5cce5dc694ff6b2a8b71c820241
Daniel <daniel.liebeskind@gmail.com>
2013-10-23 14:27:48 -0700

    Sorts links by views in descending order

--- public/client/linksView.js
+++ public/client/linksView.js
@@ -2,6 +2,8 @@ Shortly.LinksView = Backbone.View.extend({
 
   className: 'links',
 
+  orderedList: [],
+
   initialize: function(){
     this.collection.on('sync', this.addAll, this);
     this.collection.fetch();
@@ -14,11 +16,19 @@ Shortly.LinksView = Backbone.View.extend({
 
   addAll: function(){
     this.collection.forEach(this.addOne, this);
+    this.orderedList.sort(function(a, b){
+      console.log(a.model.attributes);
+      return b.model.attributes.visits - a.model.attributes.visits;
+    });
+    console.log(this.orderedList);
+    this.orderedList.forEach(function(view) {
+      this.$el.append(view.render().el);
+    }, this);
   },
 
   addOne: function(item){
     var view = new Shortly.LinkView( {model: item} );
-    this.$el.append(view.render().el);
+    console.log(view);
+    this.orderedList.push(view);
   }
-
 });
